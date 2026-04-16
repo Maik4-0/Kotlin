39d020e4f80f8e25bc3714df76e1a425b938bc47
Maik4-0 <MaikvdHeuvel@outlook.com>
2026-04-16 11:59:08 +0200

    Template

---
+++ learning-template-26.1.1/src/main/kotlin/maik/learning/LearningDataGenerator.kt
@@ -0,0 +1,9 @@
+package maik.learning
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object LearningDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
