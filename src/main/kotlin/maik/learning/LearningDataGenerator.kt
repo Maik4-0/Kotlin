fbdf312c9a5692cf729291c454875003a140493e
voidmaik <MaikvdHeuvel@outlook.com>
2026-04-22 12:18:28 +0200

    Fresh

--- src/main/kotlin/maik/learning/LearningDataGenerator.kt
+++
@@ -1,9 +0,0 @@
-package maik.learning
-
-import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
-import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
-
-object LearningDataGenerator : DataGeneratorEntrypoint {
-	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
-	}
-}
